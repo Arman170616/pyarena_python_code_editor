008cff056fb4a8667d17943cd4b4890d72b1086a
pyarena <Arman170616>
2021-03-27 09:16:52 +0600

    save function added

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ from tkinter.filedialog import asksaveasfilename, askopenfilename
 
 compiler = Tk()
 compiler.title('pyarena')
+file_path = ''
 
+
+def set_file_path(path):
+    global file_path
+    file_path = path
 
 def open_file():
     path = askopenfilename(filetypes=[('python files', '*.py')])
@@ -11,14 +16,20 @@ def open_file():
         code = file.read()
         editor.delete('1.0', END)
         editor.insert('1.0', code)
+        set_file_path(path)
         
 
 
 def save_as():
-    path = asksaveasfilename(filetypes=[('python files', '*.py')])
+    if file_path == '':
+
+        path = asksaveasfilename(filetypes=[('python files', '*.py')])
+    else:
+        path = file_path    
     with open(path, 'w') as file:
         code = editor.get('1.0', END)
         file.write(code)
+        set_file_path(path)
 
 def run():
     code = editor.get('1.0', END)
@@ -30,7 +41,7 @@ menu_bar = Menu(compiler)
 
 file_menu = Menu(menu_bar, tearoff=0)
 file_menu.add_command(label='Open', command=open_file)
-file_menu.add_command(label='Save', command=run)
+file_menu.add_command(label='Save', command=save_as)
 file_menu.add_command(label='Save As', command=save_as)
 file_menu.add_command(label='Exit', command=exit)
 menu_bar.add_cascade(label='File', menu = file_menu)  #file
--- pyarena.py
+++ pyarena.py
@@ -1,1 +1,2 @@
 print('hello arman!')
+print('hello ')
